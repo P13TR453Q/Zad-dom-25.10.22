5a3ac4a9e5dcc223df229ccd8b925073dde63a4a
1b.31.PW <114579298+P13TR453Q@users.noreply.github.com>
2022-10-25 18:49:08 +0000

    Zrobilem tyle ile umialem

---
+++ Zadanie.py
@@ -0,0 +1,42 @@
+#Zad 1
+# for i in range(1, 32):
+#   print(i, end = " ")
+
+#Zad 2
+# for i in range(1,99, 2):
+#   print((i ** 2))
+#nie wiedzialem do jakiej liczby wiec do 99 dalem
+
+#Zad 3
+# for i in range(1000, 9999):
+#   if (i % 379) == 0:
+#     print(i, end = " ")
+#Zad 4
+# for i in range(100, 999):
+#    if (i % 5) == 0 or (i % 6) == 0 or (i % 11) == 0:
+#      print(i, end = " ")
+#Zad 5
+# n = int(input())
+# for i in range(n):
+#   liczby = []
+#   suma = 0
+#   a = int(input())
+#   liczby.append(a)
+#   suma = suma + a
+# print("suma: ", suma)
+
+
+#Zad 6
+
+#Zad 7
+
+#Zad 8
+
+#Zad 9
+
+#Zad 10
+# import math
+# for i in range(1, 1000):
+#   math.sqrt(i)
+#   if (math.sqrt(i) % 0) == 0:
+#     print(math.sqrt(i))
