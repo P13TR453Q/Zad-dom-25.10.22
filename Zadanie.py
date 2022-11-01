20bdb479ce2a2767d6ac2bbde48d16b85508086a
1b.31.PW <114579298+P13TR453Q@users.noreply.github.com>
2022-11-01 18:51:54 +0000

    Zrobilem tyle ile umialem (umialem troche wiecej)

--- Zadanie.py
+++ Zadanie.py
@@ -2,7 +2,7 @@
 # for i in range(1, 32):
 #   print(i, end = " ")
 
-#Zad 2
+#Zad 2 
 # for i in range(1,99, 2):
 #   print((i ** 2))
 #nie wiedzialem do jakiej liczby wiec do 99 dalem
@@ -11,28 +11,40 @@
 # for i in range(1000, 9999):
 #   if (i % 379) == 0:
 #     print(i, end = " ")
+
 #Zad 4
 # for i in range(100, 999):
 #    if (i % 5) == 0 or (i % 6) == 0 or (i % 11) == 0:
 #      print(i, end = " ")
+
 #Zad 5
 # n = int(input())
+# suma = 0
 # for i in range(n):
-#   liczby = []
-#   suma = 0
 #   a = int(input())
-#   liczby.append(a)
-#   suma = suma + a
-# print("suma: ", suma)
-
+#   suma += a
+# print("suma to: ", suma)
 
 #Zad 6
+# k = int(input())
+# a = 0
+# for i in range(0 * 2, k * 2):
+#   a += i
+# print(a)
 
 #Zad 7
 
 #Zad 8
+# Wo = int(input())
+# L = int(input())
+# Wk = 0
+# suma = Wk
+# for i in range(0, L * 12):
+#   Wk = suma * 0.06 * (1 / 12)
+#   suma += Wk
+# print(suma)
 
-#Zad 9
+# #Zad 9
 
 #Zad 10
 # import math
